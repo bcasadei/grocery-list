96ae36f95577cd7c1b064c9bc5429fc29e82ab95
bcasadei <bill@billcasadei.com>
2015-08-08 11:07:55 -0600

    Cross off items not working and variable itemtxt isn't being defined.

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
             "<div class='title'>" +
             txtval +
             "</div><i class='fa fa-times-circle'></i></div>"+
-            "<div class='row'><p><i class='fa fa-square-o'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-times-circle'></i></p></div>" +
+            "<div class='row'><p><i class='fa fa-square-o check-box'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-plus-square-o fa-2x add-item'></i></p></div>" +
             "</div>");
         event.preventDefault();
         document.getElementById('list-name').value = '';
@@ -26,16 +26,42 @@ $(document).ready(function() {
         }
     });
 
+/* Function to get item name */
+    /*TODO variable is not being defined */
+    function newItem() {
+        var itembox = document.getElementById('item');
+        var itemtxt = itembox.value;
+        document.getElementById('item').value = '';
+        console.log(itemtxt);
+    }
+
 /* Add items to a list */
-    $(document).on( "keyup", ("#item"), function(event) {
+    /* Hit Enter key to add item */
+    $(document).on( "keyup", "#item", function(event) {
         if(event.which == 13) {
             console.log("keyup is working");
-            $(this).closest('.list').append("<div class='row'><p><i class='fa fa-square-o'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-times-circle'></i></p></div>");
+            $(newItem);
+            $(this).hide();
+            $(this).closest('.check-box').append("" + itemtxt + "");
+            $(this).closest('.list').append("<div class='row'><p><i class='fa fa-square-o check-box'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-plus-square-o fa-2x add-item'></i></p></div>");
             event.preventDefault();
         }
+    });
+    /* Hit button to add item */
+    $(document).on( "click", ".add-item", function(event) {
+        $(this).closest('.list').append("<div class='row'><p><i class='fa fa-square-o check-box'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-plus-square-o fa-2x add-item'></i></p></div>");
+        event.preventDefault();
     });
 /* Clear lists */
     $('.clear').click(function(){
         $('.list').hide();
     });
+});
+
+/* Cross off items */
+$(document).on ( "click", ".check-box", function(event) {
+    console.log("click is working");
+    $(this).removeClass(".fa-square-o").addClass(".fa-check-square-o");
+    $(this).parent(".row").addClass(".completed");
+    event.preventDefault();
 });
