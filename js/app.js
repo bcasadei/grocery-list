14604057980d478c049dba10abb8ddd8f5b24f2c
bcasadei <bill@billcasadei.com>
2015-08-08 20:23:40 -0600

    Reverted to old version. Code needs to be rewritten.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * Created by bc on 8/1/15.
  */
 $(document).ready(function() {
-/*/ Function to create and name a new list */
+    /*/ Function to create and name a new list */
     function newList() {
         var txtbox = document.getElementById('list-name');
         var txtval = txtbox.value;
@@ -12,56 +12,30 @@ $(document).ready(function() {
             "<div class='title'>" +
             txtval +
             "</div><i class='fa fa-times-circle'></i></div>"+
-            "<div class='row'><p><i class='fa fa-square-o check-box'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-plus-square-o fa-2x add-item'></i></p></div>" +
+            "<div class='row'><p><i class='fa fa-square-o'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-times-circle add-item'></i></p></div>" +
             "</div>");
         event.preventDefault();
         document.getElementById('list-name').value = '';
     }
 
-/* Event to create and name new list */
+    /* Event to create and name new list */
     $('.add').click(newList);
     $('#list-name').keydown(function(event) {
         if(event.which == 13) {
-        $(newList);
+            $(newList);
         }
     });
 
-/* Function to get item name */
-    /*TODO variable is not being defined */
-    function newItem() {
-        var itembox = document.getElementById('item');
-        var itemtxt = itembox.value;
-        document.getElementById('item').value = '';
-        console.log(itemtxt);
-    }
-
-/* Add items to a list */
-    /* Hit Enter key to add item */
-    $(document).on( "keyup", "#item", function(event) {
+    /* Add items to a list */
+    $(document).on( "keyup", ("#item"), function(event) {
         if(event.which == 13) {
             console.log("keyup is working");
-            $(newItem);
-            $(this).hide();
-            $(this).closest('.check-box').append("" + itemtxt + "");
-            $(this).closest('.list').append("<div class='row'><p><i class='fa fa-square-o check-box'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-plus-square-o fa-2x add-item'></i></p></div>");
+            $(this).closest('.list').append("<div class='row'><p><i class='fa fa-square-o'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-times-circle add-item'></i></p></div>");
             event.preventDefault();
         }
     });
-    /* Hit button to add item */
-    $(document).on( "click", ".add-item", function(event) {
-        $(this).closest('.list').append("<div class='row'><p><i class='fa fa-square-o check-box'></i> <input id='item' placeholder='Enter item name…'> <i class='fa fa-plus-square-o fa-2x add-item'></i></p></div>");
-        event.preventDefault();
-    });
-/* Clear lists */
+    /* Clear lists */
     $('.clear').click(function(){
         $('.list').hide();
     });
-});
-
-/* Cross off items - click event is working, but add/removeClass do not */
-$(document).on ( "click", ".check-box", function(event) {
-    console.log("click is working");
-    $(this).removeClass(".fa-square-o").addClass(".fa-check-square-o");
-    $(this).parent(".row").addClass(".completed");
-    event.preventDefault();
 });
